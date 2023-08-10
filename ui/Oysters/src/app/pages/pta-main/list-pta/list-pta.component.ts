204cddaf821b629d4e5a3d26dd142723241acaa4
naushada <naushad.dln@gmail.com>
2023-08-10 23:32:52 +0530

    Updated

--- ui/Oysters/src/app/pages/pta-main/list-pta/list-pta.component.ts
+++ ui/Oysters/src/app/pages/pta-main/list-pta/list-pta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IPTA } from 'src/app/core/common';
 
 @Component({
   selector: 'app-list-pta',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class ListPtaComponent {
 
+  selected:any = "";
+  year:Date = new Date();
+  ptas: Array<IPTA> = [{"year": new Date(), "pta": [{"grade": "Grade 1", "name": "ABD", "cellnumber": "1234567891", "role": "Member"},
+  {"grade": "Grade 2", "name": "XYZ", "cellnumber": "", "role": ""},
+  {"grade": "Grade 3", "name": "UVW", "cellnumber": "", "role": ""},
+  {"grade": "Grade 4", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 5", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 6", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 7", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 8", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 9", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 11", "name": "", "cellnumber": "", "role": ""}]},
+  {"year": new Date(), "pta": [{"grade": "Grade 1", "name": "ABD", "cellnumber": "1234567891", "role": "Member"},
+  {"grade": "Grade 2", "name": "XYZ", "cellnumber": "", "role": ""},
+  {"grade": "Grade 3", "name": "UVW", "cellnumber": "", "role": ""},
+  {"grade": "Grade 4", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 5", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 6", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 7", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 8", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 9", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 10", "name": "", "cellnumber": "", "role": ""},
+  {"grade": "Grade 11", "name": "", "cellnumber": "", "role": ""}]}
+]
+
+  constructor() {
+  
+  }
+
+  
+    
 }
