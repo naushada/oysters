e7403c741bb92a3fca028e6e57b3b038f56b7eb1
naushada <naushad.dln@gmail.com>
2023-08-18 19:09:02 +0530

    Updated

--- ui/Oysters/src/app/pages/grivance/list-grievance/list-grievance.component.ts
+++ ui/Oysters/src/app/pages/grivance/list-grievance/list-grievance.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IGrievance } from 'src/app/core/common';
 import { HttpService } from 'src/app/core/http.service';
 
@@ -7,12 +7,16 @@ import { HttpService } from 'src/app/core/http.service';
   templateUrl: './list-grievance.component.html',
   styleUrls: ['./list-grievance.component.scss']
 })
-export class ListGrievanceComponent {
+export class ListGrievanceComponent implements OnInit {
 
   grievances: any;
   
   constructor(private http: HttpService) {
-    http.getgrievanceinfo().subscribe((response: string) => {
+    
+  }
+
+  ngOnInit(): void {
+    this.http.getgrievanceinfo().subscribe((response: string) => {
       //this.grievances.tickets.length = 0;
       this.grievances = JSON.parse(JSON.stringify(response)) as IGrievance;
       console.log(this.grievances);
